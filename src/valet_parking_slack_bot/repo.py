d2fc50b497eb0d6117e984e455a966609298edf6
Shay Nehmad <shayn@recolabs.ai>
2021-09-17 10:07:00 +0300

    Added Repo Stub for intergration testing
    Signed-off-by: Shay Nehmad <shayn@recolabs.ai>

--- src/valet_parking_slack_bot/repo.py
+++ src/valet_parking_slack_bot/repo.py
@@ -1,5 +1,9 @@
+import random
+import logging
 from abc import ABC
 from typing import Any, Sequence
+
+logger = logging.getLogger(__name__)
 
 class ParkingSpotRepoBase(ABC):
     """Base class which defines the interface for Parking Spot Repositories."""
@@ -16,4 +20,23 @@ class ParkingSpotRepoBase(ABC):
 
     def release(self, spot_id: str) -> None:
         raise NotImplementedError()
+
+class ParkingSpotRepoStub(ParkingSpotRepoBase):
+    """Stub for repo. Behaves semi-predictibly and only used for simple 
+    integration testing. Not for production use!"""
+    spots_db = ['1', '2', '3', 'grass']
     
+    def retrieve_available_spots(self):
+        logger.info("retrieving spots")
+        return self.spots_db
+    
+    def retrieve_spots_by_user(self, username):
+        logger.info(f"retriving spots for {username}")
+        return random.choices(spots_db, k=2)
+
+    def assign(self, username, spot_id):
+        logger.info(f"assign spot {spot_id} to {username}")
+
+    def release(self, spot_id):
+        logger.info(f"releasing spot {spot_id}")
+
